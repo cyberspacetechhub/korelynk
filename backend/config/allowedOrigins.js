8badf9013a475e1d5102425b28f442a3402c3257
Cyberspace Tech Hub <esomchi8117@gmail.com>
2025-09-03 03:41:35 +0100

    changes commit

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,9 @@
 let allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3700',
-    'https://cyberspacetechhub.com',
+    'https://cyberspacetechhub.vercel.app',
     'https://www.cyberspacetechhub.com',
-    'https://cyberspace-portfolio.vercel.app'
+    'https://cyberspacetechhub.onrender.com'
 ]
 
 let environment = process.env.NODE_ENV || 'development'
--- frontend/src/config/baseUrl.js
+++ frontend/src/config/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = 'http://localhost:3700/api/'
+// const baseUrl = 'http://localhost:3700/api/'
+const baseUrl = 'https://cyberspacetechhub.onrender.com/api/'
 
 export default baseUrl
