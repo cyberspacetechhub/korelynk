794815fff9cb97f2837b6b90aa749644a7f3727f
Cyberspace Tech Hub <esomchi8117@gmail.com>
2025-11-05 11:44:45 +0100

    base url

--- frontend/src/config/baseUrl.js
+++ frontend/src/config/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3700/api/'
-// const baseUrl = 'https://korelynk.onrender.com/api/'
+// const baseUrl = 'http://localhost:3700/api/'
+const baseUrl = 'https://korelynk.onrender.com/api/'
 
 export default baseUrl
